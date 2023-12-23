360bdf1dfea0954f304a39abe2c4ede8d9a328a5
miraceti <elecam@gmail.com>
2023-12-24 00:18:20 +0100

    v71

--- pyastro7.py
+++ pyastro7.py
@@ -1,8 +1,8 @@
-from __future__ import print_function, division
+# from __future__ import print_function, division
 from astroquery.ipac.nexsci.nasa_exoplanet_archive import NasaExoplanetArchive
 
-from PyAstronomy import pyasl
-import matplotlib.pylab as plt
+# from PyAstronomy import pyasl
+# import matplotlib.pylab as plt
 import pandas as pd
 
 #recupearation all planets
@@ -11,64 +11,42 @@ import pandas as pd
 #afficher les données des exoplanetes
 print('\nNEA : TAP_TABLES :\n',NasaExoplanetArchive.TAP_TABLES)
 
-print()
-# # Get all data and plot ra vs. dec
-# dat = nexa.getAllData()
-# df = pd.DataFrame(dat)
-# print(df)
+# print(NasaExoplanetArchive.query_object("K2-18 b"))
 
-# plt.plot(dat.ra, dat.dec, 'b.')
-# plt.show()
-
-print(NasaExoplanetArchive.query_object("K2-18 b"))
+t0 =NasaExoplanetArchive.query_criteria(table="pscomppars", 
+                                        select="pl_name"
+                                        #,where="pl_name like '%Kepler-10 %'"
+                                       )
+df0= t0.to_pandas()
+print('\nNombre de lignes : ',len(df0),'\n',df0)
 
 
-nexa = pyasl.NasaExoplanetArchive()
-dat1 = nexa.getAllData()
-df1 = pd.DataFrame(dat1)
-print('DF1 : ',df1)
+
+# t3 =NasaExoplanetArchive.query_criteria(table="pscomppars", select="top 10 *")
+# df3 = t3.to_pandas()
+# colons = df3.columns.tolist()
+# colonst = colons.sort()
+# print('\nNombre de colonnes : ',len(colons),'\n', colons,'\nNombre de lignes : \n',len(df3),'\n',df3)
+
+t4 =NasaExoplanetArchive.query_criteria(table="pscomppars", 
+                                        select="pl_name,discoverymethod,disc_year,pl_bmasse"
+                                        ,where="pl_name like '%Kepler-10 %'"
+                                       )
+df4= t4.to_pandas()
+print('\nNombre de lignes : ',len(df4),'\n',df4)
 
 
-t2 =NasaExoplanetArchive.query_criteria(table="pscomppars", select="count(*)",
-                                    where="disc_facility like '%TESS%'") 
-print(t2)
+t5 =NasaExoplanetArchive.query_criteria(table="pscomppars", 
+                                        select=" distinct discoverymethod"
+                                        #,where="discoverymethod like '%imagi%'"
+                                       )
+df5= t5.to_pandas()
+print('\nNombre de lignes : ',len(df5),'\n',df5)
 
-t3 =NasaExoplanetArchive.query_criteria(table="pscomppars", select="*",
-                                    where="disc_facility like '%TESS%'") 
-print('T3 : ',t3)
-print(type(t3))
-df3 = t3.to_pandas()
-print(type(df3))
-print('DF3 : ',df3.columns)
-print(list(df3.columns.values))
-
-# Get the list of all column names from headers
-column_headers = df3.columns.values.tolist()
-print("\nThe Column Header 1 :\n", column_headers)
-
-# Using list(df) to get the column headers as a list
-column_headers = list(df3.columns)
-print("\nThe Column Header 2 :\n", column_headers)
-
-# Using list(df) to get the list of all Column Names
-column_headers = list(df3)
-print("\nThe Column Header 3 :\n", column_headers)
-
-# Dataframe show all columns sorted list
-col_headers=sorted(df3)
-print("\nThe Column Header 4 :\n", col_headers)
-
-# Get all Column Header Labels as List
-lene = 0
-for column_headers in df3.columns: 
-    lene +=1
-print("\n Nombre de colonnes 5 :\n",lene)
-
-# Get column header using keys() method  
-column_headers = df3.keys().values.tolist()
-print("\nThe Column Header 6 :\n", column_headers)
-
-# Get all numeric columns
-numeric_columns = df3._get_numeric_data().columns.values.tolist()
-print("\nThe Column Header 7 :\n", numeric_columns)
-
+t6 =NasaExoplanetArchive.query_criteria(table="pscomppars", 
+                                        select=" distinct pl_name,discoverymethod,disc_year,pl_bmasse"
+                                        ,where="discoverymethod like '%Imag%'"
+                                        ,order="disc_year desc"
+                                       )
+df6= t6.to_pandas()
+print('\nNombre de lignes : ',len(df6),'\n',df6)
